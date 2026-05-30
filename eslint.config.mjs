4d5dba7fca0dd4406ee123442ab18c4479537d05
khjasonlam <khjasonlam@gmail.com>
2026-05-31 03:14:05 +0900

    chore(eslint): update ESLint configuration to include TypeScript project settings and enforce no-deprecated rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,17 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      }
+    }
+  }
 )
+  .append({
+    rules: {
+      '@typescript-eslint/no-deprecated': 'error'
+    }
+  })
